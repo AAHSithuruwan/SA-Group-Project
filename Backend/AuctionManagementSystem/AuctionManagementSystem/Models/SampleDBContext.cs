f46bbd71412f731b50595fb91229d8bf0755f1d9
Hansa Sithuruwan <sithuruwan2002@gmail.com>
2024-10-06 21:04:09 +0530

    Creating the initial Database Connection
    Creating the User Model and migrating it to the Database

---
+++ Backend/AuctionManagementSystem/AuctionManagementSystem/Models/SampleDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AuctionManagementSystem.Models
+{
+    public class SampleDBContext : DbContext
+    {
+        public SampleDBContext(DbContextOptions<SampleDBContext> options) : base(options) { }
+
+        public virtual DbSet<User> User { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(k => k.UserId);
+            });
+        }
+    }
+}
---
+++ Backend/AuctionManagementSystem/AuctionManagementSystem/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuctionManagementSystem.Models
+{
+    public class User
+    {
+        [Key] public int UserId { get; set; }
+
+        [Required][MaxLength(50)] public required string Email { get; set; }
+
+        [Required][MaxLength(100)] public required string Password { get; set; }
+
+        [MaxLength(50)] public string? FirstName { get;  set;}
+
+        [MaxLength(50)] public string? LastName { get; set; }
+
+        [MaxLength(300)] public string? Address { get; set; }
+
+        [MaxLength(12)] public string? PhoneNumber { get; set; }
+
+    }
+}
