45cb669eb98e51676a0256033b6c4a4658f6626f
T.A.Sandali Sewmini Wishvakeerthi <sewminisandali7@gmail.com>
2024-10-21 15:33:52 +0530

    Adminauctiondetails,Categorydetails

--- Frontend/src/pages/categorydetails/categorydetails.jsx
+++ Frontend/src/pages/categorydetails/categorydetails.jsx
@@ -1,11 +1,76 @@
 import React from 'react'
+import './categorydetails.css';
+import pic1 from '../../assets/electronics.png';
+import pic2 from '../../assets/fashion.png';
+import pic3 from '../../assets/sports.png';
+
+
+const categories = [
+  {
+    id: 1,
+    image: pic1,
+    name: 'Electronic',
+  },
+  {
+    id: 2,
+    image: pic2,
+    name: 'Fashion',
+  },
+  {
+    id: 3,
+    image: pic3,
+    name: 'Sports',
+  },
+];
 
 const CategoryDetails = () => {
   return (
-    <div>
-      <h1>Category details page content</h1>
+    <div className="category-container">
+      
+
+      <div className="category-details">
+       <center> <h2>Categories Details</h2></center>
+        <button className="add-category-button">Add new category</button>
+
+        <table className="category-table">
+          <thead>
+            <tr>
+              <th>Categories</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {categories.map((category) => (
+              <tr key={category.id}>
+                <td>
+                  <img
+                    src={category.image}
+                    alt={category.name}
+                    className="category-image"
+                  />
+                  <span>{category.name}</span>
+                </td>
+                <td>
+                  <button className="edit-button">Edit</button>
+                </td>
+                <td>
+                  <button className="remove-button">Remove</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+
+
+
+
+
+
 
 export default CategoryDetails;
